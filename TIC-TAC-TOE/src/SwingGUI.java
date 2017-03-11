c07141cdd823750a4ba1a576ea14b2273bbd6e1f
leumumas <leumumassamuel@gmail.com>
2017-03-11 17:19:32 -0500

    UI avancé avec les cases et un beau fond d'écran

--- TIC-TAC-TOE/src/SwingGUI.java
+++ TIC-TAC-TOE/src/SwingGUI.java
@@ -1,60 +1,98 @@
 
 import java.awt.*;          
-import java.awt.event.*;    
+import java.awt.event.*;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;       
 import javax.swing.event.*;
-import javax.swing.BorderFactory; 
-import javax.swing.border.Border;
-import javax.swing.border.TitledBorder;
-import javax.swing.border.EtchedBorder;
-import javax.swing.ImageIcon;
-import javax.swing.JTabbedPane;
-import javax.swing.JLabel;
-import javax.swing.JPanel; 
-import javax.swing.JFrame;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
+
 
 public class SwingGUI extends JFrame {
 	
-	public JButton sendButton; 
-
-	public JTextArea txArea, rxArea;
- 	
- 	public Container container;
-     	
+	public Case [][] Cases; 
+	public JTextArea txArea, rxArea; 	
+ 	public JPanel  GridZone;	
+ 	public JPanel  Title;
+ 	public JPanel  Turn;        
+ 	public GridLayout gl;
+    private Image Background;
+    private JLabel lblBackgroundImage = new JLabel();
+    private JLabel TitleText;
+    private JLabel TurnText;
     	
+    //Construction of the UI by slice
 	public SwingGUI (String title)
 	{
-		super (title);
-		
-        container = getContentPane();             
-        container.setLayout( new FlowLayout() );
+		setTitle(title);		
+        setSize(640, 640);
+        //Set the Title Zone at the top
+        TitleText = new JLabel("Tic-Tac-Toe",
+                JLabel.CENTER);
+        TitleText.setFont(new Font("Courier New", Font.BOLD, 42));
+        TitleText.setForeground(Color.WHITE);
+        Title = new JPanel();
+        Title.setLayout( new FlowLayout() );
+        Title.setPreferredSize(new Dimension(640, 50));
+        Title.add(TitleText);
+        Title.setOpaque(false);
         
-        Border blackline, raisedetched, loweredetched,
-        raisedbevel, loweredbevel, empty;
+        //Prepare the grid
+        GridZone = new JPanel();
+        GridZone.setLayout( new FlowLayout() );
+        GridZone.setOpaque(false);
         
-        Border paneEdge = BorderFactory.createEmptyBorder(0,10,10,10);
-        JPanel simpleBorders = new JPanel();
-        simpleBorders.setBorder(paneEdge);
-        simpleBorders.setLayout(new BoxLayout(simpleBorders,
-                                              BoxLayout.Y_AXIS));
+        GridZone.add(Title);
+        Cases = new Case[3][3];
+        for (int i = 0; i < 3; i++ )
+        { 
+        	for (int j = 0; j < 3; j++ )
+        	{
+        		Case Current  = new Case (i, j);
+        		Cases[i][j] = Current;
+        		Cases[i][j].setOpaque(false);
+        		Cases[i][j].setContentAreaFilled(false);
+        		Cases[i][j].setBorderPainted(false);
+        		GridZone.add (Cases[i][j]);
+        	}
+        }
+
+        //Set the Text at the bottom of the UI
+        /*TurnText = new JLabel("Player to play",
+                JLabel.CENTER);
+        TurnText.setFont(new Font("Courier New", Font.BOLD, 42));
+        TurnText.setForeground(Color.WHITE);
+        Turn = new JPanel();
+        Turn.setLayout( new FlowLayout() );
+        Turn.setPreferredSize(new Dimension(640, 50));
+        Turn.add(TurnText);
+        Turn.setOpaque(false);        
+        Turn.add(GridZone);*/
         
-		txArea = new JTextArea (6, 40);
-		
-		rxArea = new JTextArea (6, 40);
-	
-		sendButton = new JButton ("envoyer");	
-			
-		container.add (rxArea);
-		container.add (txArea);
-		container.add (sendButton);
+        //Set the Background
+        lblBackgroundImage.setLayout(new FlowLayout());
+        try {
+        	Background = ImageIO.read(getClass().getResource("res/forest.bmp"));
+        	lblBackgroundImage.setIcon(new ImageIcon(Background));
+          } catch (Exception ex) {
+            System.out.println(ex);
+          }
+        lblBackgroundImage.setLayout(new BorderLayout());        
+        
+        lblBackgroundImage.add(GridZone);
+        add(lblBackgroundImage);
 	}
 	
 	public static void main (String[] args)
 	{
-		Frame f = new SwingGUI ("Tic-Tac-Chose");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        }
+		JFrame f = new SwingGUI ("Tic-Tac-Chose");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.pack ();
+		f.setLocationRelativeTo(null);
 		f.setVisible(true);
 	}
 
